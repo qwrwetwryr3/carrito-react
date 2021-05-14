e79757fab5de95f992b9b315d10c6358378a1074
unknown <daniel_m8c@hotmail.com>
2021-05-13 22:35:30 -0400

    Updates

---
+++ precache-manifest.ff24ff5ca74eb8a3cf188f6876b7e30f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c7954c446dea95ee179",
+    "url": "/carrito-react/static/css/main.7b9d6b7c.chunk.css"
+  },
+  {
+    "revision": "6c7954c446dea95ee179",
+    "url": "/carrito-react/static/js/main.7c6b80cb.chunk.js"
+  },
+  {
+    "revision": "c4235f28a1c24277ecd0",
+    "url": "/carrito-react/static/js/runtime~main.c5122a8b.js"
+  },
+  {
+    "revision": "0240e61efbdf18241195",
+    "url": "/carrito-react/static/css/2.73e7debb.chunk.css"
+  },
+  {
+    "revision": "0240e61efbdf18241195",
+    "url": "/carrito-react/static/js/2.73ecc71f.chunk.js"
+  },
+  {
+    "revision": "24e15524d1fd85c8292a2174454dd9b8",
+    "url": "/carrito-react/index.html"
+  }
+];
